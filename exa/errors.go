06ef5f31c5e2a33ee20bedee0549ac9861e0317e
Pawel Foremski <pjf@foremski.pl>
2025-09-26 14:31:22 +0200

    fix exabgp conversion

--- exa/errors.go
+++ exa/errors.go
@@ -13,16 +13,4 @@ var (
 	ErrInvalidNextHop   = errors.New("invalid next-hop IP address")
 	ErrInvalidOrigin    = errors.New("invalid origin")
 	ErrInvalidCommunity = errors.New("invalid community format")
-
-	// conversion errors
-	ErrNilMessage  = errors.New("nil message")
-	ErrNilLine     = errors.New("nil Exa")
-	ErrNotUpdate   = errors.New("not an UPDATE message")
-	ErrNextHopSelf = errors.New("next-hop 'self' must be resolved to actual IP before conversion")
-	ErrNoPrefix    = errors.New("no reachable or unreachable prefixes found")
-
-	// JSON errors
-	ErrParseJSON  = errors.New("parse message JSON")
-	ErrParseData  = errors.New("parse message data")
-	ErrMarshalMsg = errors.New("marshal message")
 )
